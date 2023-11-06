58684e407784555ead1a65947376d0fa5c22d08d
MelSilgon <mel.silgon@gmail.com>
2023-11-06 11:21:52 +0100

    Components Folders

--- gomoku/src/App.js
+++ gomoku/src/App.js
@@ -1,17 +1,17 @@
-import "./App.css";
-import React from "react";
-import Board from "./components/Board";
-import Navbar from "./components/NavbarComponent";
-import Footer from "./components/FooterComponent";
+import './App.css'
+import React from 'react'
+import Board from './components/Board'
+import Navbar from './components/GomokuHeader/NavbarComponent'
+import Footer from './components/GomokuFooter/FooterComponent'
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Board></Board>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Board></Board>
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
