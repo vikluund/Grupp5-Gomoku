d8a3eb9adc4d479fc5a249157d761e4390c2f23d
ShahadAlkazzaz <shahad.alqazzaz@yahoo.com>
2023-11-07 11:19:09 +0100

    created GomokuSide

--- gomoku/src/App.js
+++ gomoku/src/App.js
@@ -3,12 +3,17 @@ import React from 'react'
 import Board from './components/Board'
 import Navbar from './components/GomokuHeader/NavbarComponent'
 import Footer from './components/GomokuFooter/FooterComponent'
+import GomokuSide from './components/GomokuSide';
 
 function App() {
     return (
         <div>
             <Navbar />
-            <Board></Board>
+            {/* <Board></Board> */}
+            <div className="app-container"> {/* Använd app-container för att styra layouten */}
+        <Board></Board>
+        <GomokuSide /> {/* Placera GomokuSide till höger om Board */}
+      </div>
             <Footer />
         </div>
     )
