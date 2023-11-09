8789048c5fbff834f136dad3f3ed2a282c449ac8
ShahadAlkazzaz <shahad.alqazzaz@yahoo.com>
2023-11-09 16:13:09 +0100

    added timer

--- gomoku/src/components/GomokuSide.js
+++ gomoku/src/components/GomokuSide.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import blackPiece from "../assets/BlackPiece.png";
 import whitePiece from "../assets/WhitePiece.png";
 
 const SideContainer = styled.div`
-  display: flex;
+ display: flex;
   flex-direction: column;
   align-items: center;
 `;
@@ -13,7 +14,6 @@ const PlayerBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* margin-top: 50px; */
   background-color: var(--black-blue);
   border: 10px black;
   border-radius: 3px;
@@ -31,7 +31,7 @@ const PlayerInfo = styled.div`
 
 const PlayerName = styled.div`
   color: var(--wood-yellow);
-  margin-right: 20px; /* space mellan spelarnamn och spelarbild */
+  margin-right: 20px;
 `;
 
 const PlayerNamePlayer2 = styled(PlayerName)`
@@ -60,31 +60,71 @@ const LineSeparator = styled.div`
   margin: 20px 20px;
 `;
 
-const GomokuSide = () => {
+const GomokuSide = ({ currentPlayer, onGameEnd }) => {
+  const [player1Timer, setPlayer1Timer] = useState(60);
+  const [player2Timer, setPlayer2Timer] = useState(60);
+
+  useEffect(() => {
+    // Återställ tiden till 00:00 när det är inte spelarens tur
+    if (currentPlayer === 1) {
+      setPlayer2Timer(60);
+    } else if (currentPlayer === 2) {
+      setPlayer1Timer(60);
+    }
+  }, [currentPlayer]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentPlayer === 1 && player1Timer > 0) {
+        setPlayer1Timer((prevTime) => prevTime - 1);
+      } else if (currentPlayer === 2 && player2Timer > 0) {
+        setPlayer2Timer((prevTime) => prevTime - 1);
+      } else if (player1Timer === 0) {
+        // Spelare 2 vinner
+        onGameEnd(2);
+      } else if (player2Timer === 0) {
+        // Spelare 1 vinner
+        onGameEnd(1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentPlayer, player1Timer, player2Timer]);
+
   return (
     <SideContainer>
       <PlayerBox>
         <PlayerInfo>
-          <PlayerImage src={whitePiece} alt="Spelare 1" />
+          <PlayerImage src={blackPiece} alt="Spelare 1" />
           <PlayerName>Spelare 1:</PlayerName>
         </PlayerInfo>
-        <PlayerTimer>00:00</PlayerTimer>
+        <PlayerTimer>
+          {`${String(Math.floor(player1Timer / 60)).padStart(2, "0")}:${String(
+            player1Timer % 60
+          ).padStart(2, "0")}`}
+        </PlayerTimer>
       </PlayerBox>
       <LineSeparator />
       <PlayerBox>
         <PlayerInfo>
-          <PlayerImage src={blackPiece} alt="Spelare 2" />
+          <PlayerImage src={whitePiece} alt="Spelare 2" />
           <PlayerNamePlayer2>Spelare 2:</PlayerNamePlayer2>
         </PlayerInfo>
-        <PlayerTimer>00:00</PlayerTimer>
+        <PlayerTimer>
+          {`${String(Math.floor(player2Timer / 60)).padStart(2, "0")}:${String(
+            player2Timer % 60
+          ).padStart(2, "0")}`}
+        </PlayerTimer>
       </PlayerBox>
     </SideContainer>
   );
 };
 
+GomokuSide.propTypes = {
+  currentPlayer: PropTypes.number.isRequired,
+  onGameEnd: PropTypes.func.isRequired,
+};
+
 export default GomokuSide;
-
-
-
-
-
