2fabc6fcdf2994860b107e10d62e8958b9389e1d
Joacim <18618554+joacimstrandvide@users.noreply.github.com>
2023-11-09 13:36:59 +0100

    Games now shows in seperate board

--- gomoku/src/components/Board.js
+++ gomoku/src/components/Board.js
@@ -1,256 +1,278 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import backgroundImage from "../assets/wood.jpg";
-import blackPiece from "../assets/BlackPiece.png";
-import whitePiece from "../assets/WhitePiece.png";
-import GameOver from "./GameOver";
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import backgroundImage from '../assets/wood.jpg'
+import blackPiece from '../assets/BlackPiece.png'
+import whitePiece from '../assets/WhitePiece.png'
+import GameOver from './GameOver'
 
-const Board = () => {
-  const [gameData, setGameData] = useState(null);
-  const [winner, setWinner] = useState(null);
+const Board = ({ gamesData }) => {
+    const [gameData, setGameData] = useState(null)
+    const [winner, setWinner] = useState(null)
 
-  useEffect(() => {
-    const fetchGame = async () => {
-      try {
-        const response = await fetch(
-          "http://localhost:3000/api/gomoku/create_game"
-        );
-        const data = await response.json();
-        setGameData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchGame();
-  }, []);
+    useEffect(() => {
+        if (!gamesData) {
+            const fetchGame = async () => {
+                try {
+                    const response = await fetch(
+                        'http://localhost:3000/api/gomoku/create_game'
+                    )
+                    const data = await response.json()
+                    setGameData(data)
+                } catch (error) {
+                    console.error('Error fetching data:', error)
+                }
+            }
+            fetchGame()
+        } else {
+            setGameData(gamesData)
+        }
+    }, [gamesData])
 
-  const renderBoard = () => {
-    if (!gameData) {
-      return <div>Loading...</div>;
+    const renderBoard = () => {
+        if (!gameData) {
+            return <div>Loading...</div>
+        }
+
+        const checkWin = (tiles, player) => {
+            // Check horizontally
+            for (let row = 0; row < tiles.length; row++) {
+                for (let col = 0; col <= tiles.length - 5; col++) {
+                    if (
+                        tiles[row][col] === player &&
+                        tiles[row][col + 1] === player &&
+                        tiles[row][col + 2] === player &&
+                        tiles[row][col + 3] === player &&
+                        tiles[row][col + 4] === player
+                    ) {
+                        return true
+                    }
+                }
+            }
+
+            // Check vertically
+            for (let col = 0; col < tiles.length; col++) {
+                for (let row = 0; row <= tiles.length - 5; row++) {
+                    if (
+                        tiles[row][col] === player &&
+                        tiles[row + 1][col] === player &&
+                        tiles[row + 2][col] === player &&
+                        tiles[row + 3][col] === player &&
+                        tiles[row + 4][col] === player
+                    ) {
+                        return true
+                    }
+                }
+            }
+
+            // Check diagonally (from top-left to bottom-right)
+            for (let row = 0; row <= tiles.length - 5; row++) {
+                for (let col = 0; col <= tiles.length - 5; col++) {
+                    if (
+                        tiles[row][col] === player &&
+                        tiles[row + 1][col + 1] === player &&
+                        tiles[row + 2][col + 2] === player &&
+                        tiles[row + 3][col + 3] === player &&
+                        tiles[row + 4][col + 4] === player
+                    ) {
+                        return true
+                    }
+                }
+            }
+
+            // Check diagonally (from top-right to bottom-left)
+            for (let row = 0; row <= tiles.length - 5; row++) {
+                for (let col = 4; col < tiles.length; col++) {
+                    if (
+                        tiles[row][col] === player &&
+                        tiles[row + 1][col - 1] === player &&
+                        tiles[row + 2][col - 2] === player &&
+                        tiles[row + 3][col - 3] === player &&
+                        tiles[row + 4][col - 4] === player
+                    ) {
+                        return true
+                    }
+                }
+            }
+
+            return false
+        }
+
+        const cellClick = async (rowIndex, colIndex) => {
+            if (
+                gameData?.board?.tiles[rowIndex][colIndex] === 0 &&
+                gameData.state === 'playing'
+            ) {
+                const currentPlayer = gameData.player
+                const newTiles = gameData.board.tiles.map((row) => [...row])
+                newTiles[rowIndex][colIndex] = currentPlayer
+
+                try {
+                    const response = await fetch(
+                        'http://localhost:3000/api/gomoku/update_game',
+                        {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                board: {
+                                    tiles: newTiles
+                                },
+                                player: currentPlayer,
+                                state: gameData.state
+                            })
+                        }
+                    )
+                    const updatedGameData = await response.json()
+
+                    const nextPlayer = currentPlayer === 1 ? 2 : 1
+                    setGameData({
+                        ...updatedGameData,
+                        player: nextPlayer
+                    })
+                } catch (error) {
+                    console.error('Error updating game data:', error)
+                }
+                if (checkWin(newTiles, currentPlayer)) {
+                    setTimeout(() => {
+                        setWinner(currentPlayer)
+                    }, 100)
+                }
+            }
+        }
+
+        return gameData.board.tiles.map((row, rowIndex) => (
+            <BoardRow key={rowIndex}>
+                {row.map((col, colIndex) => (
+                    <BoardCellBackground
+                        key={colIndex}
+                        onClick={() => cellClick(rowIndex, colIndex)}
+                    >
+                        {col === 0 && <EmptyCell></EmptyCell>}
+                        {col === 1 && <Player1Cell></Player1Cell>}
+                        {col === 2 && <Player2Cell></Player2Cell>}
+                    </BoardCellBackground>
+                ))}
+            </BoardRow>
+        ))
     }
 
-    const checkWin = (tiles, player) => {
-      // Check horizontally
-      for (let row = 0; row < tiles.length; row++) {
-        for (let col = 0; col <= tiles.length - 5; col++) {
-          if (
-            tiles[row][col] === player &&
-            tiles[row][col + 1] === player &&
-            tiles[row][col + 2] === player &&
-            tiles[row][col + 3] === player &&
-            tiles[row][col + 4] === player
-          ) {
-            return true;
-          }
-        }
-      }
+    return (
+        <BoardContainer>
+            <GameName>{gameData?.name}</GameName>
+            <CurrentRound>Round: {gameData?.round}</CurrentRound>
+            {winner && <GameOver winner={winner}></GameOver>}
+            <PlayerContainer>
+                <PlayerOne className={gameData?.player === 1 ? 'active' : ''}>
+                    Player 1: {gameData?.player1.name}
+                </PlayerOne>
+                <PlayerTwo className={gameData?.player === 2 ? 'active' : ''}>
+                    Player 2: {gameData?.player2.name}
+                </PlayerTwo>
+            </PlayerContainer>
+            {renderBoard()}
+        </BoardContainer>
+    )
+}
 
-      // Check vertically
-      for (let col = 0; col < tiles.length; col++) {
-        for (let row = 0; row <= tiles.length - 5; row++) {
-          if (
-            tiles[row][col] === player &&
-            tiles[row + 1][col] === player &&
-            tiles[row + 2][col] === player &&
-            tiles[row + 3][col] === player &&
-            tiles[row + 4][col] === player
-          ) {
-            return true;
-          }
-        }
-      }
+Board.propTypes = {
+    gamesData: PropTypes.shape({
+        name: PropTypes.string,
+        round: PropTypes.number,
+        player: PropTypes.number,
+        player1: PropTypes.shape({
+            name: PropTypes.string
+        }),
+        player2: PropTypes.shape({
+            name: PropTypes.string
+        }),
+        board: PropTypes.shape({
+            tiles: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number))
+        })
+    })
+}
 
-      // Check diagonally (from top-left to bottom-right)
-      for (let row = 0; row <= tiles.length - 5; row++) {
-        for (let col = 0; col <= tiles.length - 5; col++) {
-          if (
-            tiles[row][col] === player &&
-            tiles[row + 1][col + 1] === player &&
-            tiles[row + 2][col + 2] === player &&
-            tiles[row + 3][col + 3] === player &&
-            tiles[row + 4][col + 4] === player
-          ) {
-            return true;
-          }
-        }
-      }
-
-      // Check diagonally (from top-right to bottom-left)
-      for (let row = 0; row <= tiles.length - 5; row++) {
-        for (let col = 4; col < tiles.length; col++) {
-          if (
-            tiles[row][col] === player &&
-            tiles[row + 1][col - 1] === player &&
-            tiles[row + 2][col - 2] === player &&
-            tiles[row + 3][col - 3] === player &&
-            tiles[row + 4][col - 4] === player
-          ) {
-            return true;
-          }
-        }
-      }
-
-      return false;
-    };
-
-    const cellClick = async (rowIndex, colIndex) => {
-      if (
-        gameData?.board?.tiles[rowIndex][colIndex] === 0 &&
-        gameData.state === "playing"
-      ) {
-        const currentPlayer = gameData.player;
-        const newTiles = gameData.board.tiles.map((row) => [...row]);
-        newTiles[rowIndex][colIndex] = currentPlayer;
-
-        try {
-          const response = await fetch(
-            "http://localhost:3000/api/gomoku/update_game",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                board: {
-                  tiles: newTiles,
-                },
-                player: currentPlayer,
-                state: gameData.state,
-              }),
-            }
-          );
-          const updatedGameData = await response.json();
-
-          const nextPlayer = currentPlayer === 1 ? 2 : 1;
-          setGameData({
-            ...updatedGameData,
-            player: nextPlayer,
-          });
-        } catch (error) {
-          console.error("Error updating game data:", error);
-        }
-        if (checkWin(newTiles, currentPlayer)) {
-          setTimeout(() => {
-            setWinner(currentPlayer);
-          }, 100);
-        }
-      }
-    };
-
-    return gameData.board.tiles.map((row, rowIndex) => (
-      <BoardRow key={rowIndex}>
-        {row.map((col, colIndex) => (
-          <BoardCellBackground
-            key={colIndex}
-            onClick={() => cellClick(rowIndex, colIndex)}
-          >
-            {col === 0 && <EmptyCell></EmptyCell>}
-            {col === 1 && <Player1Cell></Player1Cell>}
-            {col === 2 && <Player2Cell></Player2Cell>}
-          </BoardCellBackground>
-        ))}
-      </BoardRow>
-    ));
-  };
-
-  return (
-    <BoardContainer>
-      <GameName>{gameData?.name}</GameName>
-      <CurrentRound>Round: {gameData?.round}</CurrentRound>
-      {winner && <GameOver winner={winner}></GameOver>}
-      <PlayerContainer>
-        <PlayerOne className={gameData?.player === 1 ? "active" : ""}>
-          Player 1: {gameData?.player1.name}
-        </PlayerOne>
-        <PlayerTwo className={gameData?.player === 2 ? "active" : ""}>
-          Player 2: {gameData?.player2.name}
-        </PlayerTwo>
-      </PlayerContainer>
-      {renderBoard()}
-    </BoardContainer>
-  );
-};
-
-export default Board;
+export default Board
 
 const BoardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
-  color: white;
-  .active {
-    font-weight: bold;
-    font-size: 1.1rem;
-  }
-  width: 100%;
-`;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+    color: white;
+    .active {
+        font-weight: bold;
+        font-size: 1.1rem;
+    }
+    width: 100%;
+`
 
 const BoardCell = styled.div`
-  width: 40px;
-  height: 40px;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+    width: 40px;
+    height: 40px;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 const GameName = styled.h1`
-  color: white;
-  font-family: "gang_of_three";
-`;
+    color: white;
+    font-family: 'gang_of_three';
+`
 
 const CurrentRound = styled.p`
-  font-size: 1.5rem;
-  font-family: "gang_of_three";
-`;
+    font-size: 1.5rem;
+    font-family: 'gang_of_three';
+`
 
 const PlayerContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin: 10px;
-  font-family: "gang_of_three";
-`;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin: 10px;
+    font-family: 'gang_of_three';
+`
 
 const PlayerOne = styled.p`
-  align-self: flex-start;
-  margin-right: auto;
-  margin-left: 34vw;
-`;
+    align-self: flex-start;
+    margin-right: auto;
+    margin-left: 34vw;
+`
 
 const PlayerTwo = styled.p`
-  align-self: flex-end;
-  margin-left: auto;
-  margin-right: 34vw;
-`;
+    align-self: flex-end;
+    margin-left: auto;
+    margin-right: 34vw;
+`
 
 const BoardCellBackground = styled(BoardCell)`
-  width: 40px;
-  height: 40px;
-  background-image: url(${backgroundImage});
-  background-size: cover;
-  background-position: center;
-`;
+    width: 40px;
+    height: 40px;
+    background-image: url(${backgroundImage});
+    background-size: cover;
+    background-position: center;
+`
 
 const BoardRow = styled.div`
-  display: flex;
-`;
+    display: flex;
+`
 
 const EmptyCell = styled(BoardCell)`
-  background-color: transparent;
-`;
+    background-color: transparent;
+`
 
 const Player1Cell = styled(BoardCell)`
-  background-image: url(${blackPiece});
-  background-size: cover;
-  background-position: center;
-  background-color: transparent;
-`;
+    background-image: url(${blackPiece});
+    background-size: cover;
+    background-position: center;
+    background-color: transparent;
+`
 
 const Player2Cell = styled(BoardCell)`
-  background-image: url(${whitePiece});
-  background-size: cover;
-  background-position: center;
-  background-color: transparent;
-`;
+    background-image: url(${whitePiece});
+    background-size: cover;
+    background-position: center;
+    background-color: transparent;
+`
--- gomoku/src/components/Games.js
+++ gomoku/src/components/Games.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import PropTypes from "prop-types";
-import backgroundImage from '../assets/wood.jpg'
-import blackPiece from '../assets/BlackPiece.png'
-import whitePiece from '../assets/WhitePiece.png'
+import Board from './Board'
 
 const Games = () => {
     const [gamesData, setgamesData] = useState(null)
@@ -18,24 +16,6 @@ const Games = () => {
         } catch (error) {
             console.error('Error fetching data:', error)
         }
-    }
-
-    const renderBoard = () => {
-        if (!gamesData) {
-            return
-        }
-
-        return gamesData.board.tiles.map((row, rowIndex) => (
-            <BoardRow key={rowIndex}>
-                {row.map((col, colIndex) => (
-                    <BoardCellBackground key={colIndex}>
-                        {col === 0 && <EmptyCell></EmptyCell>}
-                        {col === 1 && <Player1Cell></Player1Cell>}
-                        {col === 2 && <Player2Cell></Player2Cell>}
-                    </BoardCellBackground>
-                ))}
-            </BoardRow>
-        ))
     }
 
     return (
@@ -54,8 +34,7 @@ const Games = () => {
                     </button>
                 </section>
             </GameButton>
-            {/* {gamesData && <Board gameData={gamesData} />} */}
-            <BoardContainer>{renderBoard()}</BoardContainer>
+            <Board gamesData={gamesData} />
         </>
     )
 }
@@ -114,56 +93,4 @@ const GameButton = styled.div`
         cursor: pointer;
         margin-bottom: 1rem;
     }
-`
-
-const BoardContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 20px;
-    color: white;
-    .active {
-        font-weight: bold;
-        font-size: 1.1rem;
-    }
-    width: 100%;
-`
-
-const BoardCell = styled.div`
-    width: 40px;
-    height: 40px;
-    border: 1px solid black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-
-const BoardCellBackground = styled(BoardCell)`
-    width: 40px;
-    height: 40px;
-    background-image: url(${backgroundImage});
-    background-size: cover;
-    background-position: center;
-`
-
-const BoardRow = styled.div`
-    display: flex;
-`
-
-const EmptyCell = styled(BoardCell)`
-    background-color: transparent;
-`
-
-const Player1Cell = styled(BoardCell)`
-    background-image: url(${blackPiece});
-    background-size: cover;
-    background-position: center;
-    background-color: transparent;
-`
-
-const Player2Cell = styled(BoardCell)`
-    background-image: url(${whitePiece});
-    background-size: cover;
-    background-position: center;
-    background-color: transparent;
 `
